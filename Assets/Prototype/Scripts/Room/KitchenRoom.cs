70d3a446dc4bb1b2015e7d2c20ba9b29cbe954ca
Aleksandar Georgiev <549190@student.saxion.nl>
2025-11-08 19:12:43 +0100

    Refactored the KitchenRoom as well

--- Assets/Prototype/Scripts/Room/KitchenRoom.cs
+++ Assets/Prototype/Scripts/Room/KitchenRoom.cs
@@ -11,154 +11,113 @@ public class KitchenRoom : DungeonRoom
     [Header("Generation settings")]
     [SerializeField] private int barSizeSpawn;
     [SerializeField] private int tableSizeSpawn;
-    [SerializeField] private int BarrelSizeSpawn;
+    [SerializeField] private int barrelSizeSpawn;
     [SerializeField, Range(1, 10)] private int barrelSpawnAmount;
 
 
 
     protected override void GenerateInterior(Room room)
     {
-
-    }
-
-    /*private void GenerateBar(Room room, int iteration)
-    {
-        if (iteration > maxPlacementIterations)
-        {
-            Debug.LogError("Fail safe while generating table/chairs");
-            return;
-        }
-
-        if (room.Width >= barSizeSpawn && room.Length >= barSizeSpawn && barPresetPrefab != null)
-        {
-            int side = Random.Range(0, 4);
-            float halfWidth = room.Width / 2f;
-            float halfLength = room.Length / 2f;
-            Vector3 barPosition = Vector3.zero;
-            Quaternion barRotation = Quaternion.identity;
-
-            switch (side)
-            {
-                case 0: // Left Center
-                    barPosition = new Vector3(room.center.x - halfWidth, 1, room.center.z);
-                    break;
-
-                case 1: // Right Center
-                    barPosition = new Vector3(room.center.x + halfWidth, 1, room.center.z);
-                    barRotation = Quaternion.Euler(0, 180, 0);
-
-                    break;
-
-                case 2: // top-left
-                    barPosition = new Vector3(room.center.x, 1, room.center.z + halfLength);
-                    barRotation = Quaternion.Euler(0, 90, 0);
-
-                    break;
-
-                case 3: // top-right
-                    barPosition = new Vector3(room.center.x, 1, room.center.z - halfLength);
-                    barRotation = Quaternion.Euler(0, -90, 0);
-                    break;
-            }
-
-            TryPlaceObject(barPresetPrefab, barPosition, barRotation);
-        }
+        GenerateBar(room, 0);
+        GenerateTableAndChairs(room, 0);
+        GenerateBarrels(room);
     }
 
     private void GenerateTableAndChairs(Room room, int iteration)
     {
         if (iteration > maxPlacementIterations)
         {
-            Debug.LogError("Fail safe while generating table/chairs");
+            Debug.LogError("Fail safe activated");
             return;
         }
 
-        if (room.Width >= tableSizeSpawn && room.Length >= tableSizeSpawn && longTablePrefabs.Length > 0 && chairPrefab != null)
+        if (longTablePrefabs.Length == 0)
         {
-            float offset = Random.Range(2, 5);
-            float halfWidth = room.Width / 2f;
-            float halfLength = room.Length / 2f;
-            int tableSpawnAmount = Random.Range(0, longTablePrefabs.Length * 2);
+            Debug.LogError("table prefabs are missing!");
+            return;
+        }
 
-            for (int i = 0; i < tableSpawnAmount; i++)
+        if (room.Width >= tableSizeSpawn && room.Length >= tableSizeSpawn)
+        {
+            GameObject tablePrefab = longTablePrefabs[Random.Range(0, longTablePrefabs.Length)];
+
+            Vector2Int tableGridSize = new Vector2Int(3, 4);
+            Vector3 tablePivotOffset = Vector3.zero;
+            int padding = 1;
+
+            Vector2Int tableGridPos;
+            Quaternion tableRotation;
+
+            if (!TryPlaceObjectInCenterArea(room, tablePrefab, tableGridSize, tablePivotOffset, padding, out tableGridPos, out tableRotation))
             {
-                GameObject tablePrefab = longTablePrefabs[Random.Range(0, longTablePrefabs.Length)];
-                float randomX = Random.Range(room.center.x - halfWidth + offset, room.center.x + halfWidth - offset);
-                float randomZ = Random.Range(room.center.z - halfLength + offset, room.center.z + halfLength - offset);
-                Vector3 randomPosition = new Vector3(randomX, 0, randomZ);
-
-                if (TryPlaceObject(tablePrefab, randomPosition, Quaternion.identity))
-                {
-                    float chairOffsetX = 1.5f;
-                    float chairOffsetZ = 1f;
-
-                    Vector3[] chairPositions =
-                    {
-                        randomPosition + new Vector3(chairOffsetX, 0, -chairOffsetZ),
-                        randomPosition + new Vector3(chairOffsetX, 0, chairOffsetZ),
-                        randomPosition + new Vector3(-chairOffsetX, 0, -chairOffsetZ),
-                        randomPosition + new Vector3(-chairOffsetX, 0, chairOffsetZ),
-                    };
-
-                    Quaternion[] chairOrientations =
-                    {
-                        Quaternion.Euler(0, 180, 0),
-                        Quaternion.Euler(0, 180, 0),
-                        Quaternion.Euler(0, 0, 0),
-                        Quaternion.Euler(0, 0, 0),
-
-                    };
-
-                    for (int j = 0; j < chairPositions.Length; j++)
-                    {
-                        TryPlaceObject(chairPrefab, chairPositions[j], chairOrientations[j]);
-                    }
-                }
+                GenerateTableAndChairs(room, ++iteration);
             }
+            else
+            {
+                Vector2Int chairGridSize = new Vector2Int(1, 1);
+                Vector3 chairPivotOffset = Vector3.zero;
+                Vector2Int rotatedTableSize = GetRotatedSize(tableGridSize, tableRotation);
+
+                int midX = tableGridPos.x + rotatedTableSize.x / 2;
+                int midY = tableGridPos.y + rotatedTableSize.y / 2;
+
+                Vector2Int bottomChairPos = new Vector2Int(midX, tableGridPos.y - 1);
+                TryPlaceObjectOnGrid(room, chairPrefab, chairGridSize, bottomChairPos, Quaternion.Euler(0, -90, 0), chairPivotOffset);
+
+                Vector2Int topChairPos = new Vector2Int(midX, tableGridPos.y + rotatedTableSize.y);
+                TryPlaceObjectOnGrid(room, chairPrefab, chairGridSize, topChairPos, Quaternion.Euler(0, 90, 0), chairPivotOffset);
+
+                Vector2Int leftChairPos = new Vector2Int(tableGridPos.x - 1, midY);
+                TryPlaceObjectOnGrid(room, chairPrefab, chairGridSize, leftChairPos, Quaternion.Euler(0, 0, 0), chairPivotOffset);
+
+                Vector2Int rightChairPos = new Vector2Int(tableGridPos.x + rotatedTableSize.x, midY);
+                TryPlaceObjectOnGrid(room, chairPrefab, chairGridSize, rightChairPos, Quaternion.Euler(0, -180, 0), chairPivotOffset);
+            }
+        }
+    }
+
+    private void GenerateBar(Room room, int iteration)
+    {
+        if (iteration > maxPlacementIterations)
+        {
+            Debug.LogError("Fail safe activated");
+            return;
+        }
+
+        if (this.barPresetPrefab == null)
+        {
+            Debug.LogError("Bar prefab are missing");
+            return;
+        }
+
+        if (room.Width >= barSizeSpawn && room.Length >= barSizeSpawn)
+        {
+            Vector2Int barGridSize = new Vector2Int(14, 7);
+            Vector3 barPivotOffset = new Vector3(0, 1f, -3.7f);
+
+            TryPlaceObjectAlongWall(room, barPresetPrefab, barGridSize, barPivotOffset, 0);
         }
     }
 
     private void GenerateBarrels(Room room)
     {
-        if (room.Width >= BarrelSizeSpawn && room.Length != BarrelSizeSpawn && barrelPrefabs != null)
+        if (this.barrelPrefabs == null)
         {
-            float halfWidth = room.Width / 2f;
-            float halfLength = room.Length / 2f;
-            float offset = 1.5f;
-            int barrelSpawnAmount = Random.Range(0, this.barrelSpawnAmount + 1);
+            Debug.LogError("Barrels prefabs are missing");
+            return;
+        }
 
-            for (int i = 0; i < barrelSpawnAmount; i++)
+        if (room.Width >= barrelSizeSpawn && room.Length >= barrelSizeSpawn)
+        {
+            GameObject barrelPrefabs = this.barrelPrefabs[Random.Range(0, this.barrelPrefabs.Length)];
+            int barrelSpawnAmount = Random.Range(0, this.barrelSpawnAmount);
+            Vector2Int barrelGridSize = new Vector2Int(2, 2);
+            Vector3 barrelPivotOffset = Vector3.zero;
+
+            for (int i = 0; i < barrelSpawnAmount; ++i)
             {
-                GameObject barrelPrefab = barrelPrefabs[Random.Range(0, longTablePrefabs.Length)];
-                int wall = Random.Range(0, 4);
-                Vector3 barrelPosition = Vector3.zero;
-                Quaternion barrelRotation = Quaternion.identity;
-
-                float randomX = Random.Range(room.center.x - halfWidth + offset, room.center.x + halfWidth - offset);
-                float randomZ = Random.Range(room.center.z - halfLength + offset, room.center.z + halfLength - offset);
-
-                switch (wall)
-                {
-                    case 0: // Top wall
-                        barrelPosition = new Vector3(randomX, 0, room.center.z + halfLength - offset);
-                        barrelRotation = Quaternion.Euler(0, 180, 0);
-                        break;
-                    case 1: // Bottom wall
-                        barrelPosition = new Vector3(randomX, 0, room.center.z - halfLength + offset);
-                        barrelRotation = Quaternion.Euler(0, 0, 0);
-                        break;
-                    case 2: // Right wall
-                        barrelPosition = new Vector3(room.center.x + halfWidth - offset, 0, randomZ);
-                        barrelRotation = Quaternion.Euler(0, -90, 0);
-                        break;
-                    case 3: // Left wall
-                        barrelPosition = new Vector3(room.center.x - halfWidth + offset, 0, randomZ);
-                        barrelRotation = Quaternion.Euler(0, 90, 0);
-                        break;
-                }
-
-                TryPlaceObject(barrelPrefab, barrelPosition, barrelRotation);
+                TryPlaceObjectAlongWall(room, barrelPrefabs, barrelGridSize, barrelPivotOffset, 0);
             }
         }
-    }*/
+    }
 }
