7a71c63bfeabd115844c360fb0a99264597cf504
Aleksandar Georgiev <549190@student.saxion.nl>
2025-12-21 23:41:29 +0100

    Starting classes for grid

---
+++ Assets/Prototype/Scripts/BSP/Grid.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    private readonly GridCell[,] cells;
+    private readonly float gridSize;
+
+    public Grid(int width, int height, float gridSize)
+    {
+        this.gridSize = gridSize;
+
+        for (int i = 0; i < height; i++)
+        {
+            for(int j = 0; j < width; j++)
+            {
+                cells[i, j] = new GridCell(i, j, gridSize);
+            }
+        }
+    }
+
+    public Vector3 GetCellPosition(int x, int z) {
+        return transform.position + cells[z, x].Position;
+    }
+}
---
+++ Assets/Prototype/Scripts/BSP/GridCell.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GridCell
+{
+    public Vector3 Position { get; }
+
+    public int X;
+    public int Z;
+    public float Size;
+    public int PrefabId;
+
+    public GridCell(int x, int z, float size)
+    {
+        X = x;
+        Z = z;
+        Size = size;
+        Position = new Vector3(x * Size + Size / 2f, z * Size + Size / 2f);
+    }
+}
